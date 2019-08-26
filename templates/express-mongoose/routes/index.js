8a8ee5ce1eac0e798d87862487ad910157648b54
Akash Rajpurohit <akash009.dev@gmail.com>
2019-08-26 20:32:06 +0530

    :checkered_flag: async await for better code readability

--- templates/express-mongoose/routes/index.js
+++ templates/express-mongoose/routes/index.js
@@ -7,14 +7,18 @@ const User = require('../models/User')
 // @route   get /index/users
 // @desc    index route
 // @access  Public
-router.get('/users', (req, res) => {
-	// return all users
-	User.find().then(user => {
-		if (!user) {
-			return res.json({ message: "No users found" })
+router.get('/users', async (req, res) => {
+	try {
+		const users = await User.find()
+
+		if(!users) {
+			return res.json({ error: false, message: "No users found" })
 		}
-		res.json({ data: user })
-	})
+		res.json({ error: false, message: "Users found!", data: users})
+
+	} catch(e) {
+		res.json({ error: true, message: "Internal Server Error" })
+	}
 })
 
 module.exports = router;
